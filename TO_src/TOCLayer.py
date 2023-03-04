397edf235b5cf52851c10df0470868bf2053bb1d
zherong <zherong@cs.unc.edu>
2023-03-04 15:53:25 -0800

    first version of TOCLayer (still buggy)

---
+++ TO_src/TOCLayer.py
@@ -0,0 +1,109 @@
+import torch
+import numpy as np
+import libMG as mg
+
+class TOCLayer(torch.autograd.Function):
+    grid = None
+    sol = None
+    b = None
+    tol = 1e-2
+    maxloop = 1000
+    output = True
+    
+    @staticmethod
+    def reset(phiTensor, phiFixedTensor, f, bb, lam, mu, tol=1e-2, maxloop=1000, output=True):
+        TOCLayer.grid = mg.GridGPU(phiTensor, phiFixedTensor, bb)
+        TOCLayer.grid.coarsen(128)
+        TOCLayer.sol = mg.GridSolverGPU(TOCLayer.grid)
+        TOCLayer.sol.setupLinearSystem(lam, mu)
+        
+        TOCLayer.b = f.cuda()
+        TOCLayer.u = torch.zeros((TOCLayer.b.shape[0],TOCLayer.b.shape[1]+1,TOCLayer.b.shape[2]+1,TOCLayer.b.shape[3]+1)).cuda()
+        TOCLayer.tol = tol
+        TOCLayer.maxloop = maxloop
+        TOCLayer.output = output
+    
+    @staticmethod
+    def forward(ctx,rho):
+        TOCLayer.solveK(rho)
+        dcc = TOCLayer.sol.sensitivityCell(TOCLayer.b,TOCLayer.u)
+        dc = TOCLayer.sol.sensitivity(TOCLayer.u)
+        ctx.save_for_backward(dc+dcc*2)
+        return -torch.sum(rho * dc)
+        
+    @staticmethod
+    def backward(ctx,coef):
+        dc=ctx.saved_tensors[0]
+        return dc*coef
+    
+    @staticmethod
+    def solveK(rho):
+        if TOCLayer.grid.isFree():
+            #TOCLayer.b = TOCLayer.sol.projectOutBases(TOCLayer.b)    (projection for b is done inside setBCell)
+            TOCLayer.u = TOCLayer.sol.projectOutBases(TOCLayer.u)
+        TOCLayer.sol.setRho(rho)
+        TOCLayer.sol.setBCell(TOCLayer.b)
+        TOCLayer.u = TOCLayer.sol.solveMGPCG(TOCLayer.u, TOCLayer.tol, TOCLayer.maxloop, True, TOCLayer.output)
+        return TOCLayer.u
+        
+def debug(iter=0, DTYPE=torch.float64):
+    bb=mg.BBox()
+    bb.minC=[-1,-1,-1]
+    bb.maxC=[1,1,1]
+    res=[10,10,15]
+    
+    def interp1D(a,b,alpha):
+        return a*(1-alpha)+b*alpha
+    def interpC(x,y,z,bb):
+        id=[x,y,z]
+        for d in range(3):
+            id[d]=interp1D(bb.minC[d],bb.maxC[d],(id[d]+0.5)/res[d])
+        return np.array(id)
+    def interpV(x,y,z,bb):
+        id=[x,y,z]
+        for d in range(3):
+            id[d]=interp1D(bb.minC[d],bb.maxC[d],id[d]/res[d])
+        return np.array(id)
+    def phi(pos):
+        #if phi(pos)<0, then this position is solid
+        return np.linalg.norm(pos)-0.5
+    def phiFixed(pos):
+        #if phiFixed(pos)<0, then this position is fixed
+        if iter==0:
+            return 1
+        else: return np.sum(pos)
+
+    phiTensor=torch.rand(tuple(res),dtype=DTYPE).cuda()
+    for z in range(res[2]):
+        for y in range(res[1]):
+            for x in range(res[0]):
+                phiTensor[x,y,z]=phi(interpC(x,y,z,bb))
+    
+    phiFixedTensor=torch.rand(tuple([res[0]+1,res[1]+1,res[2]+1]),dtype=DTYPE).cuda()
+    for z in range(res[2]+1):
+        for y in range(res[1]+1):
+            for x in range(res[0]+1):
+                phiFixedTensor[x,y,z]=phiFixed(interpV(x,y,z,bb))
+    f=torch.rand(tuple([3,res[0],res[1],res[2]]),dtype=DTYPE).cuda()
+    
+    eps = 1e-7
+    TOCLayer.reset(phiTensor, phiFixedTensor, f, bb, 100, 100, 1e-8)
+    rho = torch.rand(tuple(res),dtype=DTYPE).cuda()
+    
+    rho.requires_grad_()
+    E = TOCLayer.apply(rho)
+    E.backward()
+    
+    drho = torch.ones(tuple(res),dtype=DTYPE).cuda()
+    rho2 = rho+drho*eps
+    E2 = TOCLayer.apply(rho2)
+    numeric = (E2-E)/eps
+    analytic = torch.sum(rho.grad * drho)
+    print('TOCLayer: analytic=%f, numeric=%f, error=%f'%(analytic,numeric,analytic-numeric))
+        
+if __name__=='__main__':
+    torch.set_default_dtype(torch.float64)
+    mg.initializeGPU()
+    debug(0)
+    debug(1)
+    mg.finalizeGPU()
