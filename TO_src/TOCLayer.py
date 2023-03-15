b4eb7af2bf3414a59aff641a9ba2a89ba3ec2c98
yteng <slooowtyk@outlook.com>
2023-03-15 15:04:30 -0700

    Add testing files for TO and SO

--- TO_src/TOCLayer.py
+++ TO_src/TOCLayer.py
@@ -3,6 +3,7 @@ import numpy as np
 import libMG as mg
 from TOUtils import *
 
+
 class TOCLayer(torch.autograd.Function):
     grid = None
     sol = None
@@ -10,166 +11,134 @@ class TOCLayer(torch.autograd.Function):
     tol = 1e-2
     maxloop = 1000
     output = True
-    
+    dim = None
+
     @staticmethod
     def reset(phiTensor, phiFixedTensor, f, bb, lam, mu, tol=1e-2, maxloop=1000, output=True):
-        TOCLayer.grid = mg.GridGPU(to3DScalar(phiTensor), to3DNodeScalar(phiFixedTensor), bb)
+        TOCLayer.grid = mg.GridGPU(phiTensor, phiFixedTensor, bb)
         TOCLayer.grid.coarsen(128)
-        TOCLayer.dim = dim(phiTensor)
         TOCLayer.sol = mg.GridSolverGPU(TOCLayer.grid)
-        TOCLayer.sol.setupLinearElasticity(lam, mu, TOCLayer.dim)
-        
+        TOCLayer.sol.setupLinearElasticity(lam, mu, 3)
+        TOCLayer.dim = dim(phiTensor)
+
         TOCLayer.b = f.cuda()
-        TOCLayer.u = torch.zeros(tuple([f.shape[0]]+[f.shape[i]+1 for i in range(1,len(f.shape))])).cuda()
+        TOCLayer.u = torch.zeros(
+            (TOCLayer.b.shape[0], TOCLayer.b.shape[1] + 1, TOCLayer.b.shape[2] + 1, TOCLayer.b.shape[3] + 1)).cuda()
         TOCLayer.tol = tol
         TOCLayer.maxloop = maxloop
         TOCLayer.output = output
-    
+
     @staticmethod
-    def forward(ctx,rho):
+    def forward(ctx, rho):
         TOCLayer.solveK(rho)
-        dcc = makeSameDimScalar(TOCLayer.sol.sensitivityCell(to3DCellVector(TOCLayer.b),to3DNodeVector(TOCLayer.u)), TOCLayer.dim)
-        dc = makeSameDimScalar(TOCLayer.sol.sensitivity(to3DNodeVector(TOCLayer.u)), TOCLayer.dim)
-        ctx.save_for_backward(dc+dcc*2)
+        dcc = TOCLayer.sol.sensitivityCell(TOCLayer.b, TOCLayer.u)
+        dc = TOCLayer.sol.sensitivity(TOCLayer.u)
+        ctx.save_for_backward(dc + dcc * 2)
         return -torch.sum(rho * dc)
-        
+
     @staticmethod
-    def backward(ctx,coef):
-        dc=ctx.saved_tensors[0]
-        return dc*coef
-    
+    def backward(ctx, coef):
+        dc = ctx.saved_tensors[0]
+        return dc * coef
+
     @staticmethod
     def solveK(rho):
         if TOCLayer.grid.isFree():
-            #TOCLayer.b = makeSameDimVector(TOCLayer.sol.projectOutBases(to3DNodeVector(TOCLayer.b)), TOCLayer.dim)
-            TOCLayer.u = makeSameDimVector(TOCLayer.sol.projectOutBases(to3DNodeVector(TOCLayer.u)), TOCLayer.dim)
-        TOCLayer.sol.updateVector(to3DScalar(rho))
-        TOCLayer.sol.setBCellVector(to3DCellVector(TOCLayer.b),False)
-        TOCLayer.u = makeSameDimVector(TOCLayer.sol.solveMGPCGVector(to3DNodeVector(TOCLayer.u), TOCLayer.tol, TOCLayer.maxloop, True, TOCLayer.output), TOCLayer.dim)
+            # TOCLayer.b = TOCLayer.sol.projectOutBases(TOCLayer.b)   # (projection for b is done inside setBCell)
+            TOCLayer.u = TOCLayer.sol.projectOutBases(TOCLayer.u)
+        TOCLayer.sol.updateVector(rho)
+        TOCLayer.sol.setBCellVector(TOCLayer.b, False)
+        TOCLayer.u = TOCLayer.sol.solveMGPCGVector(TOCLayer.u, TOCLayer.tol, TOCLayer.maxloop, True, TOCLayer.output)
         if TOCLayer.grid.isFree():
-            TOCLayer.u = makeSameDimVector(TOCLayer.sol.projectOutBases(to3DNodeVector(TOCLayer.u)), TOCLayer.dim)
+            TOCLayer.u = TOCLayer.sol.projectOutBases(TOCLayer.u)
         return TOCLayer.u
-    
+
     @staticmethod
-    def nodeToCell(rho): #u->b
-        TOCLayer.sol.nodeToCellVector(TOCLayer.b,TOCLayer.u,rho)
-        
+    def nodeToCell(rho):  # u->b
+        TOCLayer.sol.nodeToCellVector(TOCLayer.b, TOCLayer.u, rho)
+
     @staticmethod
     def redistance(rho, eps=1e-3, maxIter=1000, output=False):
-        return makeSameDimScalar(TOCLayer.sol.reinitialize(rho, eps, maxIter, output), TOCLayer.dim)
-    
-def debug3D(iter=0, DTYPE=torch.float64):
-    bb=mg.BBox()
-    bb.minC=[-1,-1,-1]
-    bb.maxC=[1,1,1]
-    res=[10,10,15]
-    
-    def interp1D(a,b,alpha):
-        return a*(1-alpha)+b*alpha
-    def interpC(x,y,z,bb):
-        id=[x,y,z]
-        for d in range(3):
-            id[d]=interp1D(bb.minC[d],bb.maxC[d],(id[d]+0.5)/res[d])
-        return np.array(id)
-    def interpV(x,y,z,bb):
-        id=[x,y,z]
-        for d in range(3):
-            id[d]=interp1D(bb.minC[d],bb.maxC[d],id[d]/res[d])
-        return np.array(id)
-    def phi(pos):
-        #if phi(pos)<0, then this position is solid
-        return np.linalg.norm(pos)-0.5
-    def phiFixed(pos):
-        #if phiFixed(pos)<0, then this position is fixed
-        if iter==0:
-            return 1
-        else: return np.sum(pos)
+        return TOLayer.sol.reinitialize(rho, eps, maxIter, output)
 
-    phiTensor=torch.rand(tuple(res),dtype=DTYPE).cuda()
+    @staticmethod
+    def setupCurvatureFlow(dt, tau):
+        TOCLayer.sol.setupCurvatureFlow(dt, tau, TOCLayer.dim)
+
+    @staticmethod
+    def implicitCurvatureFlow(b, tol=1e-9, maxloop=100, MG=False):
+        if MG:
+            TOCLayer.sol.updateScalar()
+        TOCLayer.sol.setBScalar(to3DNodeScalar(b), False)
+        TOCLayer.sol.mulBScalarByNNT()
+        return makeSameDimScalar(TOCLayer.sol.solveMGPCGScalar(to3DNodeScalar(b), tol, maxloop, MG, TOCLayer.output),
+                                 TOCLayer.dim)
+
+
+def debug(iter=0, DTYPE=torch.float64):
+    bb = mg.BBox()
+    bb.minC = [-1, -1, -1]
+    bb.maxC = [1, 1, 1]
+    res = [10, 10, 15]
+
+    def interp1D(a, b, alpha):
+        return a * (1 - alpha) + b * alpha
+
+    def interpC(x, y, z, bb):
+        id = [x, y, z]
+        for d in range(3):
+            id[d] = interp1D(bb.minC[d], bb.maxC[d], (id[d] + 0.5) / res[d])
+        return np.array(id)
+
+    def interpV(x, y, z, bb):
+        id = [x, y, z]
+        for d in range(3):
+            id[d] = interp1D(bb.minC[d], bb.maxC[d], id[d] / res[d])
+        return np.array(id)
+
+    def phi(pos):
+        # if phi(pos)<0, then this position is solid
+        return np.linalg.norm(pos) - 0.5
+
+    def phiFixed(pos):
+        # if phiFixed(pos)<0, then this position is fixed
+        if iter == 0:
+            return 1
+        else:
+            return np.sum(pos)
+
+    phiTensor = torch.rand(tuple(res), dtype=DTYPE).cuda()
     for z in range(res[2]):
         for y in range(res[1]):
             for x in range(res[0]):
-                phiTensor[x,y,z]=phi(interpC(x,y,z,bb))
-    
-    phiFixedTensor=torch.rand(tuple([res[0]+1,res[1]+1,res[2]+1]),dtype=DTYPE).cuda()
-    for z in range(res[2]+1):
-        for y in range(res[1]+1):
-            for x in range(res[0]+1):
-                phiFixedTensor[x,y,z]=phiFixed(interpV(x,y,z,bb))
-    f=torch.rand(tuple([3,res[0],res[1],res[2]]),dtype=DTYPE).cuda()
-    
-    eps = 1e-7
-    TOCLayer.reset(phiTensor, phiFixedTensor, f, bb, 100, 100, 1e-8)
-    rho = torch.rand(tuple(res),dtype=DTYPE).cuda()
-    
-    rho.requires_grad_()
-    E = TOCLayer.apply(rho)
-    E.backward()
-    
-    drho = torch.ones(tuple(res),dtype=DTYPE).cuda()
-    rho2 = rho+drho*eps
-    E2 = TOCLayer.apply(rho2)
-    numeric = (E2-E)/eps
-    analytic = torch.sum(rho.grad * drho)
-    print('TOCLayer: analytic=%f, numeric=%f, error=%f'%(analytic,numeric,analytic-numeric))
-        
-def debug2D(iter=0, DTYPE=torch.float64):
-    bb=mg.BBox()
-    bb.minC=[-1,-1,0]
-    bb.maxC=[1,1,0]
-    res=[10,10]
-    
-    def interp1D(a,b,alpha):
-        return a*(1-alpha)+b*alpha
-    def interpC(x,y,bb):
-        id=[x,y]
-        for d in range(2):
-            id[d]=interp1D(bb.minC[d],bb.maxC[d],(id[d]+0.5)/res[d])
-        return np.array(id)
-    def interpV(x,y,bb):
-        id=[x,y]
-        for d in range(2):
-            id[d]=interp1D(bb.minC[d],bb.maxC[d],id[d]/res[d])
-        return np.array(id)
-    def phi(pos):
-        #if phi(pos)<0, then this position is solid
-        return np.linalg.norm(pos)-0.5
-    def phiFixed(pos):
-        #if phiFixed(pos)<0, then this position is fixed
-        if iter==0:
-            return 1
-        else: return np.sum(pos)
+                phiTensor[x, y, z] = phi(interpC(x, y, z, bb))
 
-    phiTensor=torch.rand(tuple(res),dtype=DTYPE).cuda()
-    for y in range(res[1]):
-        for x in range(res[0]):
-            phiTensor[x,y]=phi(interpC(x,y,bb))
-    
-    phiFixedTensor=torch.rand(tuple([res[0]+1,res[1]+1]),dtype=DTYPE).cuda()
-    for y in range(res[1]+1):
-        for x in range(res[0]+1):
-            phiFixedTensor[x,y]=phiFixed(interpV(x,y,bb))
-    f=torch.rand(tuple([2,res[0],res[1]]),dtype=DTYPE).cuda()
-    
+    phiFixedTensor = torch.rand(tuple([res[0] + 1, res[1] + 1, res[2] + 1]), dtype=DTYPE).cuda()
+    for z in range(res[2] + 1):
+        for y in range(res[1] + 1):
+            for x in range(res[0] + 1):
+                phiFixedTensor[x, y, z] = phiFixed(interpV(x, y, z, bb))
+    f = torch.rand(tuple([3, res[0], res[1], res[2]]), dtype=DTYPE).cuda()
+
     eps = 1e-7
     TOCLayer.reset(phiTensor, phiFixedTensor, f, bb, 100, 100, 1e-8)
-    rho = torch.rand(tuple(res),dtype=DTYPE).cuda()
-    
+    rho = torch.rand(tuple(res), dtype=DTYPE).cuda()
+
     rho.requires_grad_()
     E = TOCLayer.apply(rho)
     E.backward()
-    
-    drho = torch.ones(tuple(res),dtype=DTYPE).cuda()
-    rho2 = rho+drho*eps
+
+    drho = torch.ones(tuple(res), dtype=DTYPE).cuda()
+    rho2 = rho + drho * eps
     E2 = TOCLayer.apply(rho2)
-    numeric = (E2-E)/eps
+    numeric = (E2 - E) / eps
     analytic = torch.sum(rho.grad * drho)
-    print('TOCLayer: analytic=%f, numeric=%f, error=%f'%(analytic,numeric,analytic-numeric))
-        
-if __name__=='__main__':
+    print('TOCLayer: analytic=%f, numeric=%f, error=%f' % (analytic, numeric, analytic - numeric))
+
+
+if __name__ == '__main__':
     torch.set_default_dtype(torch.float64)
     mg.initializeGPU()
-    debug3D(0)
-    debug3D(1)
-    debug2D(1)
+    debug(0)
+    debug(1)
     mg.finalizeGPU()
--- TO_src/TestWCSO.py
+++ TO_src/TestWCSO.py
@@ -1,13 +1,16 @@
 """
-Write a script to test How WC TO could modify the surface of a given model
+Write a script to test How WC SO could modify the surface of a given model
 The input is a mesh file and the output is optimized mesh file
 """
 from TOCWorstCase import TOCWorseCase
 from TOCLayer import TOCLayer
+from TOLayer import TOLayer
 import libMG as mg
+import math
 import torch
 import numpy as np
-from TopoOpt import TopoOpt as TO
+from TOLayer import TOLayer
+from ShapeOpt import ShapeOpt as SO
 from Viewer import *
 import trimesh
 import mcubes
@@ -15,12 +18,12 @@ import time
 from TOUtils import *
 torch.set_default_dtype(torch.float64)
 
-class TestWCTO():
-###WorstCase TO taken any input mesh file as input shape
+class TestWCSO():
+###WorstCase SO taken any input mesh file as input shape
     def __init__(self, filename, margin=5, lam = 1, mu = 1):
         self.file_name = filename
         self.h = margin
-        self.rho_init, self.res, self.vol, self.phiTensor\
+        self.rho_init, self.res, self.volfrac, self.phiTensor\
             ,self.phiFixedTensor, self.rhoMask = self.initialization()
         self.lam = lam
         self.mu = mu
@@ -28,75 +31,87 @@ class TestWCTO():
 
 
 
-    def WCTO(self,p=3, rmin=1.5, maxloop=200, maxloopLinear=1000, tolx=1e-3, tolLinear=1e-2, outputInterval=1, outputDetail=False):
+    def WCSO(self,dt=0.1, tau=1e-4, p=4, d=-0.02, maxloop=200, maxloopLinear=1000, tolx=1e-3, tolLinear=1e-2, outputInterval=1, outputDetail=False, curvatureOnly=False):
         nelx, nely, nelz = self.res
         bb = mg.BBox()
         bb.minC = [0,0,0]
         bb.maxC = [nelx,nely,nelz]
-        Ker, Ker_S = TO.filter(rmin, self.rho_init)
 
-        print("Worst Case minimum complicance problem with OC and Random Initialization Loads")
+        print("Level-set shape optimization problem")
         print(f"Number of degrees:{str(nelx)} x {str(nely)} x {str(nelz)} = {str(nelx * nely * nelz)}")
-        print(f"Volume fration: {self.vol}, Penalty p: {p}, Filter radius: {rmin}")
+        print(f"Volume fration: {self.volfrac}")
         # max and min stiffness
         E_min = torch.tensor(1e-3)
         E_max = torch.tensor(1.0)
         change = tolx*2
+        nvol = maxloop // 2
         loop = 0
         g=0
 
-        #compute filtered volume gradient (this is contant so we can precompute)
-        rho = self.rho_init.detach()
-        rho.requires_grad_()
-        rho_filtered = TO.filter_density(Ker, rho)/Ker_S
-        volume = torch.sum(rho_filtered)
-        volume.backward()
-        gradVolume = rho.grad.detach()
-
+        phi = -self.rho_init.clone()
+        strength = (SO.nodeToCell(phi) > 0).double().detach()
         #initialize torch layer
         mg.initializeGPU()
         Initial_f = torch.rand((3, *self.rho_init.shape)).cuda()
-        # Initial_f = torch.zeros((3, *self.rho_init.shape)).cuda()
-        # Initial_f[1, -1, 0, :] = -1
-        # self.phiFixedTensor[0, :, :] = -1
+        showRhoVTK('fWCPhi_init_SO', phi.detach().cpu().numpy())
+
         TOCLayer.reset(self.phiTensor, self.phiFixedTensor, Initial_f, bb, self.lam, self.mu, tolLinear, maxloopLinear, outputDetail)
-        rho_filtered = TO.filter_density(Ker, rho) / Ker_S
-        TOCWorseCase.compute_worst_case(E_min + rho_filtered ** p * (E_max - E_min), eps=1e-2, maxloop=50)
+        TOCLayer.setupCurvatureFlow(dt, tau * nelx * nely * nelz)
+        TOCWorseCase.compute_worst_case(strength * (E_max - E_min) + E_min, eps=1e-2, maxloop=50)
         print_f = TOCLayer.b.detach().cpu().numpy()
-        showFMagnitudeCellVTK("fWorstCell", print_f)
+        showFMagnitudeCellVTK("fWorstCell_SO", print_f)
         while change > tolx and loop < maxloop:
             start = time.time()
             loop += 1
 
-            # compute filtered objective gradient
-            rho = rho.detach()
-            rho.requires_grad_()
-            rho_filtered = TO.filter_density(Ker, rho) / Ker_S
-            obj = TOCLayer.apply(E_min + rho_filtered ** p * (E_max - E_min))
-            obj.backward()
-            gradObj = rho.grad.detach()
+            #compute volume
+            strength = (SO.nodeToCell(phi)>0).double().detach()
+            vol = torch.sum(strength).item() / strength.reshape(-1).shape[0]
+            if loop == 1:
+                volInit = vol
+                if self.volfrac is None:
+                    self.volfrac = volInit
 
-            rho_old = rho.clone()
-            rho, g = TO.oc_grid(rho, gradObj, gradVolume, g, self.rhoMask)
-            change = torch.linalg.norm(rho.reshape(-1, 1) - rho_old.reshape(-1, 1), ord=float('inf')).item()
+            # FE-analysis, calculate sensitivity
+            if curvatureOnly:
+                dir = torch.ones(strength.shape).cuda()
+                obj = 0
+            else:
+                strength.requires_grad_()
+                obj = TOCLayer.apply(strength * (E_max - E_min) + E_min)
+                # simple replacement of topological derivative
+                obj.backward()
+                dir = (-strength.grad.detach() * (strength * (E_max - E_min) + E_min)).detach()
+            dirNode = SO.cellToNode(dir)
+
+            # set augmented Lagrangian parameter
+            ex = self.volfrac + (volInit - self.volfrac) * max(0, 1 - loop / nvol)
+            lam = torch.sum(dirNode) / dirNode.reshape(-1).shape[0] * math.exp(p * ((vol - ex) / ex + d))
+
+            # update level set function
+            phi_old = phi.clone()
+            C = dir.reshape(-1).shape[0] / torch.sum(torch.abs(dirNode)).item()
+            phi = TOCLayer.implicitCurvatureFlow(C * (dirNode - lam) + phi / dt)
+            phi = torch.minimum(torch.tensor(1.), torch.maximum(torch.tensor(-1.), phi))
+            change = torch.linalg.norm(phi.reshape(-1, 1) - phi_old.reshape(-1, 1), ord=float('inf')).item()
             end = time.time()
             if loop % outputInterval == 0:
-                print("it.: {0}, obj.: {1:.3f}, vol.: {2:.3f}, ch.: {3:.3f}, time: {4:.3f}, mem: {4:.3f}Gb".format(loop,
+                print("it.: {0}, obj.: {1:.3f}, vol.: {2:.3f}, ch.: {3:.3f}, time: {4:.3f}, mem: {5:.3f}Gb".format(loop,
                                                                                                                    obj,
-                                                                                                                   (
-                                                                                                                               g + self.vol * nelx * nely * nelz) / (
+                                                                                                                   torch.sum(
+                                                                                                                       strength).item() / (
                                                                                                                                nelx * nely * nelz),
                                                                                                                    change,
                                                                                                                    end - start,
                                                                                                                    torch.cuda.memory_allocated(
                                                                                                                        None) / 1024 / 1024 / 1024))
-            rho = torch.maximum(rho, torch.tensor(0.001))
-            # TOCWorseCase.compute_worst_case(E_min + rho_filtered ** p * (E_max - E_min),eps=1e-2,maxloop=50)
+            # rho = torch.maximum(rho, torch.tensor(0.001))
+            # TOCWorseCase.compute_worst_case(rho)
             # TOCWorseCase.update_worst_case(rho)
-        showFMagnitudeCellVTK("fWorstCell_final", TOCLayer.b.detach().cpu().numpy())
-        showRhoVTK('fWCRho',rho_old.detach().cpu().numpy())
+        showFMagnitudeCellVTK("fWorstCell_final_SO", TOCLayer.b.detach().cpu().numpy())
+        showRhoVTK('fWCPhi_SO',phi.detach().cpu().numpy())
         mg.finalizeGPU()
-        return to3DScalar(rho_old).detach().cpu().numpy()
+        return to3DScalar(phi_old).detach().cpu().numpy()
 
 
     def initialization(self):
@@ -117,50 +132,35 @@ class TestWCTO():
         # # _ = mesh.export('test.obj')
         # mesh.show()
 
-
-
-
-        Rho = self.dumbbell()
-        Rho = np.maximum(Rho, 0.001)
+        Rho = self.dumbbell_SO()
+        Rho = np.minimum(Rho, 1)
         res = Rho.shape
-        volfrac = Rho.sum() / (res[0] * res[1] * res[2])
-
+        # volfrac = Rho.sum() / (res[0] * res[1] * res[2])
 
         rho = torch.from_numpy(Rho).cuda()
-        # res = [180,60,4]
-        # rho = 0.3*torch.ones(res).cuda()
-        # volfrac = 0.3
         nelx, nely, nelz = res
-        phiTensor = -torch.ones_like(rho).cuda()
+        nelx = nelx-1
+        nely = nely-1
+        nelz = nelz-1
+        phiTensor = -torch.ones((nelx, nely, nelz)).cuda()
         phiFixedTensor = torch.ones((nelx + 1, nely + 1, nelz + 1)).cuda()
-
-
-        # import pickle
-        # file = open('data.txt', 'wb')
-        # pickle.dump(rho, file)
-        # file.close()
-        # file = open('data.txt', 'rb')
-        # # dump information to that file
-        # aaa = pickle.load(file)
-        # # close the file
-        # file.close()
 
         def rhoMask(inputRho):
             pass
-        return rho, res, volfrac, phiTensor, phiFixedTensor, rhoMask
+        return rho, res, None, phiTensor, phiFixedTensor, rhoMask
 
-    def dumbbell(self):
+    def dumbbell_SO(self):
         center = [15, 15, 15]
         width = 10
-        X, Y, Z = np.meshgrid(np.arange(0, 60), np.arange(0, 60), np.arange(0, 60))
+        X, Y, Z = np.meshgrid(np.arange(0, 61), np.arange(0, 61), np.arange(0, 61))
         dist = np.max([np.abs(X - center[0]), np.abs(Y - center[1]), np.abs(Z - center[2])], axis=0)
-        cube = np.zeros_like(dist)
-        cube[dist <= width / 2] = 1
+        cube = np.ones_like(dist)
+        cube[dist <= width / 2] = -2
 
         center2 = [45, 45, 15]
         dist = np.max([np.abs(X - center2[0]), np.abs(Y - center2[1]), np.abs(Z - center2[2])], axis=0)
-        cube2 = np.zeros_like(dist)
-        cube2[dist <= width / 2] = 1
+        cube2 = np.ones_like(dist)
+        cube2[dist <= width / 2] = -2
 
         Cube = cube + cube2
 
@@ -168,14 +168,14 @@ class TestWCTO():
             for j in range(-3, 3):
                 for k in range(center[2] - 2, center[2] + 2):
                     # if i+j<=center[0]+center2:
-                    Cube[i, i + j, k] = 1
+                    Cube[i, i + j, k] = -1
         return Cube.astype(np.float64)
 
 if __name__ == "__main__":
     # file = './data/stanford-bunny.obj'
     file = 'dumbbell.obj'
-    TT = TestWCTO(file)
-    a = TT.WCTO(maxloop=5)
-    torch.save(a, "dumbbell.pt")
-    # a = torch.load("dumbbell.pt")
+    TT = TestWCSO(file)
+    a = TT.WCSO(maxloop=10)
+    torch.save(a, "dumbbell_SO.pt")
+    # a = torch.load("dumbbell_SO.pt")
     showRho(a, 0.99)
