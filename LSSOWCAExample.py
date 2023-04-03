3e28a1ae4cd9b819914aae5e96e74dc5cdf9610b
yteng <slooowtyk@outlook.com>
2023-04-03 13:36:48 -0700

    Use Mask to identify phiFixed. Connector between two balls preserved but rho value is only around 0.2

--- LSSOWCAExample.py
+++ LSSOWCAExample.py
@@ -1,8 +1,8 @@
 from TO_src.LevelSetShapeOptWorstCaseApproximation import LevelSetShapeOptWorstCaseApproximation
 import torch,os
 import numpy as np
-import pysdf
-import skimage
+# import mesh_to_sdf as mts
+# import skimage
 
 def TWOballs_Example_3D(res=(100, 100, 100)):
     nelx, nely, nelz = res
@@ -36,7 +36,7 @@ def TWOballs_Example_3D(res=(100, 100, 100)):
     mu = 1 / 2.6
     return res, (phiTensor, phiFixedTensor, lam, mu, phi, phi0)
 
-def Dumbbell_Example_3D(res=(100, 100, 100)):
+def Dumbbell_Example_3D(res=(100, 100, 100), r=2, l=1):
     nelx, nely, nelz = res
     phiTensor = -torch.ones(res).cuda()
     phiFixedTensor = torch.ones((nelx + 1, nely + 1, nelz + 1)).cuda()
@@ -58,8 +58,8 @@ def Dumbbell_Example_3D(res=(100, 100, 100)):
 
     Ball = cube + cube2
     for i in range(center[0], center2[0]):
-        for j in range(-2, 2):
-            for k in range(center[2] - 1, center[2] + 1):
+        for j in range(-r, r):
+            for k in range(center[2] - r, center[2] + r):
                 # if i+j<=center[0]+center2:
                 Ball[i, i + j, k] = -1
     # import mcubes
@@ -68,43 +68,79 @@ def Dumbbell_Example_3D(res=(100, 100, 100)):
     # mesh = trimesh.Trimesh(vertices, triangles)
     # # _ = mesh.export('test.obj')
     # mesh.show()
+
     phi0 = torch.from_numpy(Ball).double().cuda()
     center3 = np.array(center)//2+np.array(center2)//2
     dist_init = (X - center3[0]) ** 2 + (Y - center3[1]) ** 2 + (Z - center3[2]) ** 2
     R_min = np.maximum(np.sqrt((center2[0]-center3[0])**2+(center2[1]-center3[1])**2+(center2[2]-center3[2])**2), np.sqrt((center[0]-center3[0])**2+(center[1]-center3[1])**2+(center[2]-center3[2])**2))
     Init = np.ones_like(dist_init)
-    Init[dist_init<=(R_min+width+3)**2] = -1
+    Init[dist_init<=(R_min+width+5)**2] = -1
     phi = torch.from_numpy(Init).double().cuda()
     lam = 0.3 / 0.52
     mu = 1 / 2.6
-    return res, (phiTensor, phiFixedTensor, lam, mu, phi, phi0)
 
-def Dumbbell_Example_3D_case2(res=(100, 100, 100)):
+    center = [40, 40, 40]
+    width = 10
+    X, Y, Z = np.meshgrid(np.arange(0, 101), np.arange(0, 101), np.arange(0, 101))
+    dist = (X - center[0]) ** 2 + (Y - center[1]) ** 2 + (Z - center[2]) ** 2
+    cube = np.ones_like(dist)
+    cube[dist <= width ** 2] = -1
+
+    center2 = [60, 60, 40]
+    dist = (X - center2[0]) ** 2 + (Y - center2[1]) ** 2 + (Z - center2[2]) ** 2
+    cube2 = np.zeros_like(dist)
+    cube2[dist <= width ** 2] = -2
+
+    Ball = cube + cube2
+    Ball[Ball==1] = -2
+    Ball[Ball== -1] = 1
+    for i in range(center[0], center2[0]):
+        for j in range(-r-l, r+l):
+            for k in range(center[2] - r-l, center[2] + r+l):
+                # if i+j<=center[0]+center2:
+                Ball[i, i + j, k] = 1
+    Mask = torch.from_numpy(Ball).double().cuda()
+    return res, (phiTensor, phiFixedTensor, lam, mu, phi, phi0, Mask)
+
+def Dumbbell_Example_3D_case2(res=(55, 55, 55), r=2):
     nelx, nely, nelz = res
     phiTensor = -torch.ones(res).cuda()
     phiFixedTensor = torch.ones((nelx + 1, nely + 1, nelz + 1)).cuda()
     phi = torch.ones([nelx+1, nely+1, nelz+1]).cuda()
+    # phiFixedTensor[:30,:30,60:]=-1
+    # phiFixedTensor[60:, :30,60:] = -1
+    # phiFixedTensor[:30,:30,:10]=-1
+    # phiFixedTensor[10:40,: ,10:40] = 1
 
-    center = [10, 10, 40]
-    width = 5
-    X, Y, Z = np.meshgrid(np.arange(0, 101), np.arange(0, 101), np.arange(0, 101))
+    center = [15, 25, 25]
+    width = 10
+    X, Y, Z = np.meshgrid(np.arange(0, 56), np.arange(0, 56), np.arange(0, 56))
     dist = (X - center[0])**2 + (Y - center[1])**2 + (Z - center[2])**2
     cube = np.ones_like(dist)
     cube[dist<=width**2] = -1
+    ff = np.zeros((3,56,56,56))
+    temp = np.zeros_like(dist)
+    temp[dist<=width**2] = 1
+    ff[0,:,:,:] = temp
 
-    center2 = [90, 90, 40]
+    center2 = [40, 25, 25]
     dist = (X - center2[0])**2 + (Y - center2[1])**2 + (Z - center2[2])**2
     cube2 = np.zeros_like(dist)
     cube2[dist<=width**2] = -2
-
+    temp2 = np.zeros_like(dist)
+    temp2[dist<=width**2] = -1
+    ff[0,:,:,:] = ff[0,:,:,:]+temp2
 
 
     Ball = cube + cube2
-    for i in range(center[0], center2[0]):
-        for j in range(-2, 2):
-            for k in range(center[2] - 2, center[2] + 2):
-                # if i+j<=center[0]+center2:
-                Ball[i, i + j, k] = -1
+    if r==0:
+        pass
+    else:
+        for i in range(center[0], center2[0]):
+            for j in range(-r, r):
+                for k in range(center[2] - r, center[2] + r):
+                    # if i+j<=center[0]+center2:
+                    Ball[i, i + j, k] = -1
     # import mcubes
     # import trimesh
     # vertices, triangles = mcubes.marching_cubes(Ball, -0.99)
@@ -118,19 +154,22 @@ def Dumbbell_Example_3D_case2(res=(100, 100, 100)):
     Init = np.ones_like(dist_init)
     Init[dist_init<=(R_min+width+3)**2] = -1
     phi = torch.from_numpy(Init).double().cuda()
-    lam = 0.3 / 0.52
-    mu = 1 / 2.6
-    return res, (phiTensor, phiFixedTensor, lam, mu, phi0, phi0)
+    lam = 1
+    mu = 10
+    return res, (phiTensor, phiFixedTensor, lam, mu, ff, phi0)
 
-# def combine_two_mesh():
-#     mesh = trimesh.load('aaa.obj')
-#
-#     sdf = pysdf.SDF(mesh.vertices, mesh.faces)
-#
-#     vertices, triangles = mcubes.marching_cubes(sdf, 0.0)
-#     mesh = trimesh.Trimesh(vertices, triangles)
-#     # _ = mesh.export('test_init.obj')
-#     mesh.show()
+def combine_two_mesh():
+    mesh = trimesh.load('data/f16/f16.obj')
+
+    sdf = mts.mesh_to_voxels(mesh, 400, pad=False)
+    torch.save(sdf, "data/f16/f16.pt")
+    vertices, triangles = mcubes.marching_cubes(sdf, 0.0)
+    mesh = trimesh.Trimesh(vertices, triangles)
+    # _ = mesh.export('test_init.obj')
+    mesh.show()
+
+# write a function here to generate a sdf model which has a mapping mask for evaluating the original sdf and
+#     the desired output for an optimization goal for our final target,
 
 
 if __name__ == "__main__":
@@ -148,15 +187,17 @@ if __name__ == "__main__":
 
 
 
-    # _, params = TWOballs_Example_3D()
-    _, params = TWOballs_Example_3D()
-    sol = LevelSetShapeOptWorstCaseApproximation(dt=0.001, tau=1e-6,outputDetail=False, maxloop=200)
-    phi = sol.run(*params)
+    _, params = Dumbbell_Example_3D(r=0, l=3)
+    # _, params = combine_two_mesh()
+    sol = LevelSetShapeOptWorstCaseApproximation(h=1, dt=0.1, tau=1e-6,outputDetail=False, maxloop=100)
+    phi, rho = sol.run(*params)
     torch.save(phi,"phi.pt")
-    # phi=torch.load("phi.pt")
+
+
+    phi=torch.load("phi.pt")
     #
-    # vertices, triangles = mcubes.marching_cubes(phi, 0.01)
-    # mesh = trimesh.Trimesh(vertices, triangles)
-    # _ = mesh.export('test.obj')
+    vertices, triangles = mcubes.marching_cubes(rho, 0.01)
+    mesh = trimesh.Trimesh(vertices, triangles)
+    _ = mesh.export('test.obj')
     # mesh.show()
     showRhoVTK("phi", phi, False)
--- TO_src/LevelSetShapeOptWorstCaseApproximation.py
+++ TO_src/LevelSetShapeOptWorstCaseApproximation.py
@@ -12,7 +12,7 @@ class LevelSetShapeOptWorstCaseApproximation(LevelSetShapeOpt):
         LevelSetShapeOpt.__init__(self, h=h, dt=dt, tau=tau, maxloop=maxloop, maxloopLinear=maxloopLinear, tolx=tolx,
                                   tolLinear=tolLinear, outputInterval=outputInterval, outputDetail=outputDetail)
 
-    def run(self, phiTensor, phiFixedTensor, lam, mu, phi, phi0):
+    def run(self, phiTensor, phiFixedTensor, lam, mu, phi, phi0, mask0):
         nelx, nely, nelz = shape3D(phiTensor)
         nelz = max(nelz, 1)
         bb = mg.BBox()
@@ -21,13 +21,14 @@ class LevelSetShapeOptWorstCaseApproximation(LevelSetShapeOpt):
         #Now phi is pregenerated, we will write a function to automatically generate it.
         volTarget = torch.sum(phi0 < 0).item()
 
-
         # Ker, Ker_S = SIMPTopoOpt.filter(2.5, phi)
+        phiFixedTensor = phiFixedTensor * (-mask0)
 
 
         print("Worst case level-set shape optimization problem")
         print(f"Number of degrees:{str(nelx)} x {str(nely)} x {str(nelz)} = {str(nelx * nely * nelz)}")
         print(f"Volume target: {volTarget}, Tau={self.tau}")
+        showRhoVTK("results/mask_000", to3DNodeScalar(mask).detach().cpu().numpy(), False)
         # max and min stiffness
         E_min = torch.tensor(1e-3)
         E_max = torch.tensor(1.0)
@@ -42,6 +43,7 @@ class LevelSetShapeOptWorstCaseApproximation(LevelSetShapeOpt):
             f = torch.zeros((2, nelx, nely))
         else:
             f = torch.zeros((3, nelx, nely, nelz))
+        # showFMagnitudeCellVTK("TwoForce_finit", f.detach().cpu().numpy())
         TOCLayer.reset(phiTensor, phiFixedTensor, f, bb, lam, mu, self.tolLinear, self.maxloopLinear, self.outputDetail)
         TOCLayer.setupCurvatureFlow(self.dt, self.tau * nelx * nely * nelz)
         phi = TOCLayer.reinitializeNode(phi)
@@ -54,14 +56,24 @@ class LevelSetShapeOptWorstCaseApproximation(LevelSetShapeOpt):
 
             # update worst case
             rho = LevelSetShapeOpt.compute_density(phi, self.h)
-            if loop == 1:
+            # TOCWorstCase.compute_worst_case(rho * (E_max - E_min) + E_min)
+            if loop % 10 == 1:
+                torch.save(phi, f"phi{loop}.pt")
+                torch.save(rho, f"rho{loop}.pt")
                 TOCWorstCase.compute_worst_case(rho * (E_max - E_min) + E_min)
                 # showFMagnitudeVTK("phi_f", TOCLayer.b.detach().cpu().numpy())
             else:
                 TOCWorstCase.update_worst_case(rho * (E_max - E_min) + E_min)
-                # pass
+                pass
             # TOCLayer.fix()
+            # u = TOCLayer.solveK(rho * (E_max - E_min) + E_min).detach().cpu().numpy()
+            # f = TOCLayer.bOut.detach().cpu().numpy()
+            #
+            # showFMagnitudeCellVTK("TwoForce_u", u)
+            # showFMagnitudeCellVTK("TwoForce_f", f)
 
+            # Eng = np.matmul(f.flatten().T , u.flatten())
+            # showFMagnitudeCellVTK("Eng_r0", u*f)
             # compute volume
             phi = phi.detach()
             phi.requires_grad_()
@@ -69,40 +81,43 @@ class LevelSetShapeOptWorstCaseApproximation(LevelSetShapeOpt):
             vol.backward()
             gradVolume = phi.grad.detach()
 
-
-
+            # [318.3081178563203, 448038.18137584074]
+            # [307.93483162973877, 444902.9132840459]
+            # [314.7987559758582, 438083.48916411755]
             # FE-analysis, calculate sensitivity, add L^2 loss in shape difference
             phi = phi.detach()
             phi.requires_grad_()
+
             SD_loss = torch.nn.MSELoss(reduction='sum')(LevelSetShapeOpt.compute_density(phi, self.h), rho0)
-            c = 0.05
+            c = 0
             SP_loss = TOCLayer.apply(LevelSetShapeOpt.compute_density(phi, self.h) * (E_max - E_min) + E_min)
             if SD_loss > c*volTarget:
-                b = 1*SP_loss.item() / (SD_loss.item()+0.01)
+                b = 0.1*SP_loss.item() / (SD_loss.item()+0.01)
             else:
                 b = 0
                 print('Now there is no shape similarity loss')
             obj = SP_loss +  b * SD_loss
             obj.backward()
             gradObj = -phi.grad.detach()
-            if SD_loss > c*volTarget:
-                pass
-            else:
-                self.dt = 0.1
-                gradObj, vol = LevelSetShapeOpt.find_lam(phi, gradObj, gradVolume, volTarget, self.h, self.dt)
-                print('Now we apply lam')
+            # if SD_loss > c*volTarget:
+            #     pass
+            # else:
+                # self.dt = 0.1
+            # gradObj, vol = LevelSetShapeOpt.find_lam(phi, gradObj, gradVolume, volTarget, self.h, self.dt)
+                # print('Now we apply lam')
 
             # update level set function / reinitialize
             phi_old = phi.clone()
             phi = TOCLayer.implicitCurvatureFlow(gradObj + phi / self.dt)
             phi = TOCLayer.reinitializeNode(phi)
             change = torch.linalg.norm(phi.reshape(-1, 1) - phi_old.reshape(-1, 1), ord=float('inf')).item()
+
             end = time.time()
             if loop % self.outputInterval == 0:
                 if not os.path.exists("results"):
                     os.mkdir("results")
-                print("it.: {0}, obj.: {1:.3f}, SD_loss.: {2:.3f}, vol.: {3:.3f}, ch.: {4:.3f}, time: {5:.3f}, mem: {6:.3f}Gb".format(loop,
-                                                                                                                   obj,
+                print("it.: {0}, SP_loss.: {1:.3f}, SD_loss.: {2:.3f}, vol.: {3:.3f}, ch.: {4:.3f}, time: {5:.3f}, mem: {6:.3f}Gb".format(loop,
+                                                                                                                   SP_loss,
                                                                                                                    SD_loss,
                                                                                                                    vol,
                                                                                                                    change,
@@ -110,6 +125,6 @@ class LevelSetShapeOptWorstCaseApproximation(LevelSetShapeOpt):
                                                                                                                    torch.cuda.memory_allocated(
                                                                                                                        None) / 1024 / 1024 / 1024))
                 showRhoVTK("results/rho" + str(loop), to3DNodeScalar(rho).detach().cpu().numpy(), False)
-        showFMagnitudeVTK("phi_f_final", TOCLayer.b.detach().cpu().numpy())
+        # showFMagnitudeVTK("phi_f_final", TOCLayer.b.detach().cpu().numpy())
         mg.finalizeGPU()
-        return to3DNodeScalar(phi_old).detach().cpu().numpy()
+        return to3DNodeScalar(phi_old).detach().cpu().numpy(), rho.detach().cpu().numpy()
